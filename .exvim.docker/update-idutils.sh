338d1e8fe27c562f510e60dc0aefd56bf24cecb2
qren <ren552118004@163.com>
2019-07-30 16:07:47 +0800

    typos

--- .exvim.docker/update-idutils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.docker"
-export TOOLS="/Users/joshtrick/.vim/tools/"
-export TMP="${DEST}/_ID"
-export TARGET="${DEST}/ID"
-sh ${TOOLS}/shell/bash/update-idutils.sh
--- .exvim.docker/update-symbols.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.docker"
-export TOOLS="/Users/joshtrick/.vim/tools/"
-export TMP="${DEST}/_symbols"
-export TARGET="${DEST}/symbols"
-sh ${TOOLS}/shell/bash/update-symbols.sh
